225d92ccd18226d59f988424b6ad2b2515be561d
Christopher Petersen <chris@petersen.io>
2020-10-06 10:03:16 -0700

    Little script works

--- powercount.rb
+++ powercount.rb
@@ -1,6 +1,12 @@
+require 'csv'
 require 'ruby_powerpoint'
 
-files = Dir.glob("#{ENV["HOME"]}/**/*.pptx?")
+files = Dir.glob("#{ENV["HOME"]}/**/*.ppt*")
 files.each do |file|
-  puts file
-end
+  # next unless file.match(/\.pptx?$/i)
+  next unless file.match(/\.pptx$/i)
+  next if file.include?('~$')
+  next if file.include?('[Autosaved]')
+  deck = RubyPowerpoint::Presentation.new file
+  puts CSV.generate_line([file, deck.slides.count])
+end;nil
